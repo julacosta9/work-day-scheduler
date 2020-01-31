2b71e432c0139b0803372e38df63a36004c517c6
Julian Acosta <julacosta9@gmail.com>
2020-01-31 15:09:59 -0800

    Update organization so code flows logically, use ternary operator to declare isAM variable

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,24 @@
 $(document).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
     // update app with data from local storage
     for (let i = 0; i <= 8; i++) {
         let storedItem = localStorage.getItem("hour-" + i);
         $("#hour-" + i + "-text").val(storedItem);
     }
 
-    // save data in field to local storage. delete localStorage key if field is empty
-    function saveText(event) {
-        let target = event.target;
-        let keyName;
-        let textareaValue;
-        let rowElement;
-        console.log(target);
-
-        if (target.tagName == "DIV") {
-            textareaValue = target.previousElementSibling.children[0].value;
-            rowElement = target.parentElement.getAttribute("data-index");
-            keyName = "hour-" + rowElement;
-        } else {
-            textareaValue =
-                target.parentElement.previousElementSibling.children[0].value;
-            rowElement = target.parentElement.parentElement.getAttribute(
-                "data-index"
-            );
-            keyName = "hour-" + rowElement;
-        }
-
-        if (textareaValue !== "") {
-            localStorage.setItem(keyName, textareaValue);
-        } else {
-            localStorage.removeItem(keyName);
-        }
-    }
-
     // Change background color of hour rows based on time of day
-    $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
-
     let currentHour = moment()
         .format("LT")
         .split(":")
         .shift();
-
-    let isAM;
 
     let ampm = moment()
         .format("LT")
         .split(" ")
         .pop();
 
-    if (ampm == "AM") {
-        isAM = true;
-    } else {
-        isAM = false;
-    }
+    let isAM = (ampm == "AM") ? true : false;
 
     if (isAM) {
         for (let i = 3; i <= 8; i++) {
@@ -64,9 +30,8 @@ $(document).ready(function() {
             let hourRow = $(".hour[data-index=" + i + "]");
             if (hourRow.attr("data-hour") == currentHour) {
                 hourRow.next().addClass("present");
-            }
-            // need to use parseInt so that the integer values are compared instead of the string chacter values
-            else if (
+            } else if (
+                // need to use parseInt so that the integer values are compared instead of the string chacter values
                 parseInt(hourRow.attr("data-hour")) > parseInt(currentHour)
             ) {
                 hourRow.next().addClass("future");
@@ -124,6 +89,32 @@ $(document).ready(function() {
         }
     }
 
-    // event listeners
+    // save data in field to local storage. delete localStorage key if field is empty
+    function saveText(event) {
+        let target = event.target;
+        let keyName;
+        let textareaValue;
+        let rowElement;
+
+        if (target.tagName == "DIV") {
+            textareaValue = target.previousElementSibling.children[0].value;
+            rowElement = target.parentElement.getAttribute("data-index");
+            keyName = "hour-" + rowElement;
+        } else {
+            textareaValue =
+                target.parentElement.previousElementSibling.children[0].value;
+            rowElement = target.parentElement.parentElement.getAttribute(
+                "data-index"
+            );
+            keyName = "hour-" + rowElement;
+        }
+
+        if (textareaValue !== "") {
+            localStorage.setItem(keyName, textareaValue);
+        } else {
+            localStorage.removeItem(keyName);
+        }
+    }
+
     $(".saveBtn").on("click", saveText);
 });
